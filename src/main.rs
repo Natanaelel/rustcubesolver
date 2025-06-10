a8cea6a9e0fc77f618b4f4312b3b20730586457d
Natanaelel <43886350+Natanaelel@users.noreply.github.com>
2025-06-10 14:46:47 +0200

    measure time

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 mod cube;
 mod solver;
 
+use std::time::Instant;
+
 use cube::move_sequence::Sequence;
 
 use cube::Cube;
 
 fn main() {
-    let scramble = Sequence::random_scramble(5);
+    // let scramble = Sequence::random_scramble(5);
+    let scramble = Sequence::parse(String::from("R U R' U' R")).unwrap();
     println!("Finding solution for {}", scramble);
 
+    let start_time = Instant::now();
     let solution = solver::solve(Cube::create_solved().apply(&scramble));
+    let time_taken = Instant::now() - start_time; // 6.143426903s --release: 387.960225ms
+
     if let Some(sequence) = solution {
-        println!("Found solution: {}", sequence);
+        println!("Found solution in {:?}: {}", time_taken, sequence);
         print_link(scramble, sequence);
     } else {
         println!("Didn't find any solution");
